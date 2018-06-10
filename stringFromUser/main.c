9b0d874ec91dd4c058aa1de202fcdf282beadda3
Nathan Wainwright <nathanwainwright@gmail.com>
2018-06-10 00:38:01 -0700

    Initial Commit

---
+++ stringFromUser/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  stringFromUser
+//
+//  Created by Nathan Wainwright on 2018-06-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
